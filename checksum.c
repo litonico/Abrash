6ddcb88a14567c51056023eb4b0b138d068c9a39
Lito Nicolai <lito.nicolai@gmail.com>
2016-05-18 18:28:28 -0700

    Checksum with getc

--- checksum.c
+++ checksum.c
@@ -4,31 +4,26 @@
 
 int main(int argc, char *argv[])
 {
-    int Handle;
-    unsigned char Byte;
+    FILE *CheckFile;
+    int Byte;
     unsigned int Checksum;
-    int ReadLength;
 
     if ( argc != 2 ) {
         printf("usage: checksum filename\n");
         return 1;
     }
 
-    Handle = open(argv[1], O_RDONLY);
-    if ( Handle == -1 ) {
+    CheckFile = fopen(argv[1], "rb");
+    if (CheckFile == NULL) {
         printf("Can't open file %s\n", argv[1]);
     }
 
     Checksum = 0;
 
-    ReadLength = read(Handle, &Byte, sizeof(Byte));
-    while (ReadLength > 0) {
+    Byte = getc(CheckFile);
+    while (Byte != EOF) {
         Checksum += (unsigned int)Byte;
-        ReadLength = read(Handle, &Byte, sizeof(Byte));
-    }
-    if ( ReadLength == -1 ) {
-        printf("Error reading file %s\n", argv[1]);
-        return 1;
+        Byte = getc(CheckFile);
     }
 
     printf("The checksum is: %u\n", Checksum);
