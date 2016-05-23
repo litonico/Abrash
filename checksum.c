cd823e49f047d820a268f15c314e1b18d09553b0
Lito Nicolai <lito.nicolai@gmail.com>
2016-05-23 00:18:22 -0700

    Use our own buffer

--- checksum.c
+++ checksum.c
@@ -1,31 +1,49 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BUFFER_SIZE 0x8000 // 32kb
+
 int main(int argc, char *argv[])
 {
-    FILE *CheckFile;
-    int Byte;
+    int Handle;
     unsigned int Checksum;
+    unsigned char *WorkingBuffer, *WorkingPtr;
+    int WorkingLength, LengthCount;
 
     if ( argc != 2 ) {
         printf("usage: checksum filename\n");
         return 1;
     }
 
-    CheckFile = fopen(argv[1], "rb");
-    if (CheckFile == NULL) {
+    Handle = open(argv[1], O_RDONLY);
+    if (Handle == -1) {
         printf("Can't open file %s\n", argv[1]);
+        return 1;
+    }
+
+    WorkingBuffer = malloc(BUFFER_SIZE);
+
+    if ( WorkingBuffer == NULL ) {
+        printf("Not enough memory!");
+        return 1;
     }
 
     Checksum = 0;
 
-    Byte = getc(CheckFile);
-    while (Byte != EOF) {
-        Checksum += (unsigned int)Byte;
-        Byte = getc(CheckFile);
-    }
-
+    do {
+        WorkingLength = read(Handle, WorkingBuffer, BUFFER_SIZE);
+        if ( WorkingLength == -1 ) {
+            printf("error reading file");
+        }
+        WorkingPtr = WorkingBuffer;
+        LengthCount = WorkingLength;
+        while ( LengthCount-- ) { // Walk through the buffer
+            Checksum += (unsigned int)*WorkingPtr;
+            WorkingPtr++;
+        }
+    } while ( WorkingLength );
     printf("The checksum is: %u\n", Checksum);
     return 0;
 }
